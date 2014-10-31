dd82450aa8b2eabd85464d699c45a83121871934
Joseph Schilz <joseph@schilz.org>
2014-10-31 12:27:38 -0700

    Updated version number.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-mashup',
-    version='0.1',
+    version='0.11',
     packages=['mashup'],
     include_package_data=True,
     license='GNU General Public License v3 (GPLv3)',
